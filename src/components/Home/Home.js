e5047a0ea972a194098edeb28c2b8b290a23a74a
kibria khandaker <46866083+kibria-khandaker@users.noreply.github.com>
2022-04-29 16:40:17 +0600

    client-side data fetch from Sever ( Local and Hiroku  both) Done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [fruits, setFruits] = useState([]);
+    useEffect(()=>{
+
+        // heroku server tested link = working
+        // const url = `https://nameless-bastion-84935.herokuapp.com/product`;
+
+        // http://localhost:5000/product server link = working
+        const url = `http://localhost:5000/product`;
+
+        fetch(url)
+        .then(res=>res.json())
+        .then(data=>setFruits(data))
+    },[]);
+
+    console.log(fruits);
     return (
         <div>
-            Home page
+            Home page {fruits.length}
         </div>
     );
 };
